2b34f6a980ef844612ab5e1c6e78595b459daa28
Akash patel <akashpatel10000@gmail.com>
2021-10-23 13:43:05 +0530

    Created React app from Create React app command, Created initial file Structure and Created Home Route for Home Page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="Home">
+            <h2>Welcome to the Home</h2>
+        </div>
+    )
+}
+
+export default Home
