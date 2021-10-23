d17de382c72864f4c7eabb5147692a410f4af48c
Akash patel <akashpatel10000@gmail.com>
2021-10-23 22:09:09 +0530

    Created Responsive Navbar Component and render it to Home Page Component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Router>
         <Switch>
           <Route path="/" component={Home} exact/>
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import './Navbar.css';
+import React, {useState} from 'react'
+
+// Other Imports
+import {Link} from 'react-router-dom'
+import MenuIcon from '@material-ui/icons/Menu';
+
+export default function Navbar() {
+    // State To Manage Menu Toggle
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="navbar">
+            <h2 className="navbarLogo">Akash Patel</h2>
+            <ul className="navbarLinks" style={{transform: open ? "translateX(0px)" : ""}}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/project">Projects</Link></li>
+                <li><Link to="/experience">Experience</Link></li>
+                <li><Link to="/skills">Skills</Link></li>
+                <li><Link to="/constact">Contact</Link></li>
+            </ul>
+            <MenuIcon className="navbarIcon" onClick={() => setOpen(!open)} />
+        </div>
+    )
+}
